57904958b6be754947dec1ef41d212cebebe2a52
Silas Skov Christensen <120374711+Lynsild55@users.noreply.github.com>
2025-05-07 01:22:35 +0200

    Update RestartSpotify.ps1
    clean up

--- RestartSpotify.ps1
+++ RestartSpotify.ps1
@@ -1,13 +1,10 @@
-# Kill all Spotify-related processes
 Get-Process | Where-Object { $_.Name -like "Spotify*" } | Stop-Process -Force -ErrorAction SilentlyContinue
 
 Start-Sleep -Seconds 2
 
-# Start Spotify
 Start-Process "$env:APPDATA\Spotify\Spotify.exe"
 
-# Play
-Start-Sleep -Seconds 5  # wait for Spotify to finish loading
+Start-Sleep -Seconds 5  
 
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait(" ")  # spacebar simulates Play/Pause
